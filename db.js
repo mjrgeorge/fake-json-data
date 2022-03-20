f10f4c3b52a46b52586af0f1e41c60a89bb3f000
Jubayer Rahman <mjrgeorge@gmail.com>
2022-03-20 22:51:04 +0600

    new book data added

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
-const items = require("./data/items");
-const testingItems = require("./data/testingItems");
+const book_all_data = require("./data/book_all_data");
+const book_chapter_contents = require("./data/book_chapter_contents");
+const book_chapter_lesson = require("./data/book_chapter_lesson");
+const book_lesson_contents = require("./data/book_lesson_contents");
 
 module.exports = {
-  items,
-  testingItems
+  book_all_data,
+  book_chapter_contents,
+  book_chapter_lesson,
+  book_lesson_contents
 }
